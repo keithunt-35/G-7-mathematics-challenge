b1c42b5d9efed69cd10ffc445ad70b22333ca211
keithunt-35 <keithunt35@duck.com>
2024-07-25 22:28:09 +0300

    questions migration

--- database/migrations/2024_07_23_203442_create_questions_table.php
+++ database/migrations/2024_07_23_203442_create_questions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2024_07_23_203442_create_questions_table.php
         Schema::create('questions', function (Blueprint $table) {            
             $table->unsignedInteger('challengeId');
             $table->foreign('challengeId')->references('challengeId')->on('challenges');
@@ -21,15 +20,7 @@ return new class extends Migration
             $table->unsignedInteger('questionId')->primary();
             $table->string('questionText');            
             $table->timestamps();
-========
-        Schema::create('questions', function (Blueprint $table) {
-             
-            $table->integer('challengeId')->unsigned();
-            $table->foreign('challengeId')->references('challengeId')->on('_challenges');
-            $table->integer('marks',3)->nullable(false);
-            $table->integer('questionId')->primary();
-            $table->string('questionText',255)->nullable(false);
->>>>>>>> b3687b9efd2836b87236fad87d0ef4a3df29f7d1:database/migrations/2024_07_24_182815_create_questions_table.php
+
         });
     }
 
