6a9f84db5b007923a3fc8dbddbbccbe4d39be1bc
keithunt-35 <keithunt35@duck.com>
2024-07-25 22:29:52 +0300

    questions migration

--- database/migrations/2024_07_23_203716_create_answers_table.php
+++ database/migrations/2024_07_23_203716_create_answers_table.php
@@ -15,19 +15,14 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->integer('answerId')->primary();
-<<<<<<<< HEAD:database/migrations/2024_07_23_203716_create_answers_table.php
+
             $table->unsignedInteger('questionId');
             $table->foreign('questionId')->references('questionId')->on('questions');
             $table->string('text');
             $table->boolean('isCorrect');
             $table->timestamps();            
-========
-            $table->integer('questionId')->unsigned();
-            $table->foreign('questionid')->references('questionId')->on('_questions')->onDelete('cascade');
-            $table->string('text');
-            $table->boolean('is_Correct');
-          
->>>>>>>> b3687b9efd2836b87236fad87d0ef4a3df29f7d1:database/migrations/2024_07_24_183048_create_answers_table.php
+
+
         });
     }
 
