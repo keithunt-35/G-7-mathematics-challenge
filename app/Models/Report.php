9df2e36022a1ad692a265d89ddbf93670949c08f
keithunt_35 <144621721+keithunt-35@users.noreply.github.com>
2024-07-25 08:54:29 +0300

    Update Report.php

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'reportId',
+        'participantId',
+        'challengeId'
+    ];
+
+    public function participant()
+    {
+        return $this->belongsTo('App\Models\Participant');
+    }
+
+    public function challenge()
+    {
+        return $this->belongsTo('App\Models\Challenge');
+    }
+
 }
