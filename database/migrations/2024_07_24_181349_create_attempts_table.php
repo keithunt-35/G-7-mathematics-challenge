eeb6902d185112b0248610646d366e41860f398e
keithunt_35 <144621721+keithunt-35@users.noreply.github.com>
2024-07-25 22:35:43 +0300

    Update 2024_07_24_181349_create_attempts_table.php
    repeated function

--- database/migrations/2024_07_24_181349_create_attempts_table.php
+++ database/migrations/2024_07_24_181349_create_attempts_table.php
@@ -4,8 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+
     /**
      * Run the migrations.
      *
@@ -25,17 +25,6 @@ return new class extends Migration
             $table->foreign('challengeId')->references('challengeId')->on('challenges')->onDelete('cascade');
 
             $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('');
         });
     }
 
