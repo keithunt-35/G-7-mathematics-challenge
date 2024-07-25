3f62859a67b43a2b5f773b00f15e3ced866ad59e
keithunt_35 <144621721+keithunt-35@users.noreply.github.com>
2024-07-25 08:50:31 +0300

    Update Participant.php

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -7,5 +7,59 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
-    use HasFactory;
+    // Specify guarded attributes to prevent mass assignment vulnerabilities
+    protected $guarded = [];
+
+    // Specify fillable attributes for mass assignment
+    protected $fillable = [
+        'dateOfBirth',
+        'email',
+        'firstName',
+        'lastName',
+        'password',
+        'schoolId',
+        'username'
+    ];
+
+    // Define the table associated with the model
+    protected $table = 'participants';
+
+    // Define the primary key associated with the table
+    protected $primaryKey = 'participantId';
+
+    // Set the timestamp format
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    // Define the relationships
+    public function school()
+    {
+        return $this->belongsTo('App\Models\School');
+    }
+
+    public function challenges()
+    {
+        return $this->belongsToMany('App\Models\Challenge')->withPivot('noOfQuestions', 'duration', 'startDate', 'endDate');
+    }
+
+    public function answers()
+    {
+        return $this->hasMany('App\Models\Answer');
+    }
+
+    public function attempts()
+    {
+        return $this->hasMany('App\Models\Attempt');
+    }
+
+    public function reports()
+    {
+        return $this->hasMany('App\Models\Report');
+    }
+
+    public function verifiedParticipant()
+    {
+        return $this->hasOne('App\Models\VerifiedParticipant');
+    }
+  
+
 }
