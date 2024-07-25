d09ae24ca7fca59f13967274d64e3d91105445a2
keithunt_35 <144621721+keithunt-35@users.noreply.github.com>
2024-07-25 08:48:24 +0300

    Update Challenge.php

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -7,12 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Challenge extends Model
 {
+    
     protected $table = '_challenges';
-    protected $fillable = ['name', 'challengeId', 'startDate', 'endDate', 'duration', 'noOfQuestions'];
-
+    protected $fillable = [
+        'name', 
+        'challengeId', 
+        'startDate',
+        'endDate',
+        'duration',
+        'noOfQuestions'
+    ];
+ 
     public function questions()
-{
-    return $this->hasMany(Question::class);
-}
+    {
+        return $this->hasMany('App\Models\Question');
+    }
+
+    public function participants()
+    {
+        return $this->belongsToMany('App\Models\Participant')->withPivot('noOfQuestions', 'duration', 'startDate', 'endDate');
+    }
+
 
 }
