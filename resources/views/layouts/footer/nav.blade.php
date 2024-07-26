a96fe9bf75f8217283a2d018280e47b382a0c91d
Jerome-B-Nuwagaba <Nuwagababill5@gmail.com>
2024-07-26 12:42:58 +0300

    Update nav.blade.php
    Updated the footer

--- resources/views/layouts/footer/nav.blade.php
+++ resources/views/layouts/footer/nav.blade.php
@@ -1,6 +1,7 @@
 <footer class="footer">
     <div class="container @auth-fluid @endauth">
         <nav>
+           <!-- Don't want this in our footer
             <ul class="footer-menu">
                 <li>
                     <a href="https://www.creative-tim.com" class="nav-link" target="_blank">{{ __('Creative Tim') }}</a>
@@ -14,13 +15,13 @@
                 <li>
                     <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">{{ __('Blog') }}</a>
                 </li>
-            </ul>
+            </ul> -->
             <p class="copyright text-center">
                 ©
                 <script>
                     document.write(new Date().getFullYear())
                 </script>
-                <a href="http://www.creative-tim.com">{{ __('Creative Tim') }}</a> &amp; <a href="https://www.updivision.com">{{ __('Updivision') }}</a> {{ __(', made with love for a better web') }}
+                <a href="http://www.creative-tim.com">{{ __('G-7') }}</a> &amp; <a href="https://www.updivision.com">{{ __('Mathematics Challenge') }}</a> {{ 'Unlocking the Powerof Maths!') }}
             </p>
         </nav>
     </div>
