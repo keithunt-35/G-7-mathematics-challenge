adb1f477bf2ecd86c4dfeed7b0377bf0144a56e5
Jerome-B-Nuwagaba <Nuwagababill5@gmail.com>
2024-07-26 13:30:15 +0300

    Update sidebar.blade.php

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="sidebar" data-image="{{ asset('light-bootstrap/img/sidebar-5.jpg') }}">
+<div class="sidebar" data-image="{{ asset('light-bootstrap/img/sidebar-4.jpg') }}">
     <!--
 Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
 
